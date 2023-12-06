6b696c914796f5c1d760da9a5840a14543886dca
amine759 <saihiamine569@gmail.com>
2023-12-06 19:22:41 +0100

    pub sub

--- publisher.py
+++ publisher.py
@@ -4,6 +4,9 @@ import json
 import redis
 import asyncio
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 redis_host = os.environ.get("redis_host")
 channel1 = os.environ.get("channel_stream1")
@@ -12,53 +15,47 @@ redis_port = os.environ.get("redis_port")
 
 stream1 = "wss://stream.binance.com:9443/ws/btcusdt@kline_1s"  # streaming in 1s mode -> metrics : n trades per seconds, Event time
 stream2 = "wss://stream.binance.com:9443/ws/btcusdt@trade"  # streaming in trade mode -> metrics : btc price , event time
-
 """
 Data stored in time series in redis --> then connected to grafana
 """
 
-# r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
 
 
 def unix_to_time(time):
     seconds = time / 1000
-    normal_time = datetime.utcfromtimestamp(seconds).strftime('%Y-%m-%dT%H:%M:%S')
+    normal_time = datetime.utcfromtimestamp(seconds).strftime("%Y-%m-%dT%H:%M:%S")
     return normal_time
 
 
-async def publish_channel1(stream):
-    # Process received WebSocket messages here
-
-    """Process and store data in Redis time series
-    Example: Storing data in Redis with a timestamp as key"""
+async def publish_to_redis(stream, channel):
     async with websockets.connect(stream) as ws:
-        # Stay alive forever, listening to incoming msgs
-        while True:
-            msg = await ws.recv()
-            data = json.loads(msg)
-            event_time = unix_to_time(data.get("E"))  # E: Event timestamp
-            n_trades = data.get("k").get("n")  # E: number of transactions
-            # r.set(event_time, n_trades)
-            # r.set(event_time, btc_price)  # Store the message using the timestamp as key in Redis
-            print(event_time, n_trades)
+            while True:
+                msg = await ws.recv()
+                # Assuming the received message is JSON
+                data = json.loads(msg)
+                event_time = unix_to_time(data.get("E"))  # Event timestamp
 
+                # Example data points to store in Redis
+                if channel == channel1:
+                    n_trades = data.get("k").get("n")  # Number of transactions
+                    data = json.dumps({"event_time": event_time,"n_trades": n_trades})
+                    r.set(channel2,data)
+                    print("published : ",data)
 
-async def publish_channel2(stream):
-    async with websockets.connect(stream) as ws:
-        # Stay alive forever, listening to incoming msgs
-        while True:
-            msg = await ws.recv()
-            data = json.loads(msg)
-            event_time = unix_to_time(data.get("E"))  # E: Event timestamp
-            # r.set(event_time, n_trades)
-            btc_price = data.get("p")  # p: bitcoin price in the timestamp
-            # r.set(event_time, btc_price)  # Store the message using the timestamp as key in Redis
-            print(event_time, btc_price)
-
+                elif channel == channel2:
+                    btc_price = data.get("p")  # Bitcoin price
+                    data = json.dumps({"event_time": event_time,"bitcoin_price": btc_price})
+                    r.set(channel2,data)
+                    print("published : ",data)
 
 async def main():
-    await asyncio.gather(publish_channel1(stream1), publish_channel2(stream2))
+    # Run coroutines to publish to Redis channels
+    await asyncio.gather(
+        publish_to_redis(stream1, channel1), publish_to_redis(stream2, channel2)
+    )
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
--- subscriber.py
+++ subscriber.py
@@ -1,22 +1,33 @@
+import os
+import asyncio
 import redis
+from dotenv import load_dotenv
 
-class Subscriber:
-    def __init__(self):
-        self.redis_host = 'localhost'
-        self.redis_port = 6379
+load_dotenv()
+# Retrieving environment variables
+redis_host = os.environ.get("redis_host")
+channel1 = os.environ.get("channel_stream1")
+channel2 = os.environ.get("channel_stream2")
+redis_port = os.environ.get("redis_port")
 
-    def subscribe_channel(self):
-        redis_client = redis.StrictRedis(host=self.redis_host, port=self.redis_port)
+# Redis connection setup
+r = redis.StrictRedis(host=redis_host, port=redis_port)
+
+async def subscribe_to_channel(channel):
+    pubsub = r.pubsub()
+    pubsub.subscribe(channel)
     
-        # Subscribe to the channel named 'pub_sub_channel'
-        pub_sub = redis_client.pubsub()
-        pub_sub.subscribe('pub_sub_channel')
+    for message in pubsub.listen():
+        # Process received message
+        if message['type'] == 'message':
+            print(f"Received message from {channel}: {message['data'].decode('utf-8')}")
 
-        # Start listening for messages
-        for message in pub_sub.listen():
-            if message['type'] == 'message':
-                print(f"Received message: {message['data'].decode('utf-8')}")
+async def main():
+    # Run coroutines to subscribe to Redis channels
+    await asyncio.gather(
+        subscribe_to_channel(channel1),
+        subscribe_to_channel(channel2)
+    )
 
 if __name__ == "__main__":
-    subscriber = Subscriber()
-    subscriber.subscribe_channel()
+    asyncio.run(main())
